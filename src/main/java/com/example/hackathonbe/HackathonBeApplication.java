951d7b4f770716bf61952859536d87830309c1e3
EunJi <univ217@naver.com>
2025-07-17 14:33:03 +0900

    :gear:Setting

---
+++ src/main/java/com/example/hackathonbe/HackathonBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackathonbe/HackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
